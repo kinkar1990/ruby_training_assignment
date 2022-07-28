059f12ad7a1180ac93fe55f00c2662740123043f
Kinkar Paul <kpaul@sproutloud.com>
2022-07-28 16:33:07 +0530

    Electricity Consumption

--- electricity_consumption.rb
+++ electricity_consumption.rb
@@ -1,51 +1,55 @@
 class ElectricityConsumption
-    @@userDeatils = []
-    def calculateCharges(inputUserName, inputUnitsConsumed)
-        if(inputUnitsConsumed <= 100)
-            @totalCharges = inputUnitsConsumed*6
-        elsif(inputUnitsConsumed > 100 && inputUnitsConsumed <= 300)
-            @remainingUnit = inputUnitsConsumed - 100
-            @totalCharges = 100*6 + @remainingUnit*11
+    attr_accessor :input_user_name, :input_units_consumed
+    @@user_deatils = []
+    def initialize(input_user_name, input_units_consumed)
+        @input_user_name = input_user_name
+        @input_units_consumed = input_units_consumed
+    end
+    def calculate_charges
+        case @input_units_consumed
+        when 1..99
+            @total_charges = @input_units_consumed*6
+        when 100..300
+            @remainingUnit = @input_units_consumed - 100
+            @total_charges = 100*6 + @remainingUnit*11
         else
-            @remainingUnit = inputUnitsConsumed - 300 
-            @totalCharges = 100*6 + 200*11 + @remainingUnit*15
+            @remainingUnit = @input_units_consumed - 300 
+            @total_charges = 100*6 + 200*11 + @remainingUnit*15
         end
-        if(@totalCharges > 300)
-            @totalCharges = @totalCharges + (15*@totalCharges)/100
+        case @total_charges
+        when 1..300
+            @total_charges = @total_charges + (5*@total_charges)/100
         else
-            @totalCharges = @totalCharges + (5*@totalCharges)/100
+            @total_charges = @total_charges + (15*@total_charges)/100
         end
-        if(@totalCharges < 50)
-            @totalCharges = 50
-        end
-        @@userDeatils.push({"inputUserName" => inputUserName,
-        "inputUnitsConsumed" => inputUnitsConsumed,
-        "totalCharges" => @totalCharges.to_f.round(2)
+        @@user_deatils.push({"input_user_name" => input_user_name,
+        "input_units_consumed" => @input_units_consumed,
+        "total_charges" => @total_charges < 50 ? 50.00 : @total_charges.to_f.round(2)
     })
     end
-    def displayUserUnitsConsumed
+    def display_user_units_consumed
         i = 0
-        @@userDeatils.each_with_index do |user, i|
-            puts " #{i + 1} | #{user["inputUserName"]}  | #{user["inputUnitsConsumed"]}  |  #{user["totalCharges"]}"
+        @@user_deatils.each_with_index do |user, i|
+            puts " #{i + 1} | #{user["input_user_name"]}  | #{user["input_units_consumed"]}  |  #{user["total_charges"]}"
         end
     end
 
 end
 
-electricityConsumption = ElectricityConsumption.new
 puts "Enter the number of user"
-inputUserCount = gets.chomp.to_i
-if(inputUserCount > 0)
+input_user_count = gets.chomp.to_i
+if(input_user_count > 0)
     i = 1
-    while i <= inputUserCount
+    while i <= input_user_count
         puts "Enter the name of user"
-        inputUserName = gets.chomp
+        input_user_name = gets.chomp
         puts "Enter the units consumed by user"
-        inputUnitsConsumed = gets.chomp.to_i
-        electricityConsumption.calculateCharges(inputUserName, inputUnitsConsumed)
+        input_units_consumed = gets.chomp.to_i
+        electricity_consumption = ElectricityConsumption.new(input_user_name, input_units_consumed)
+        electricity_consumption.calculate_charges
         i = i + 1
     end
-    electricityConsumption.displayUserUnitsConsumed
+    electricity_consumption.display_user_units_consumed
 else
     puts "Please enter correct input"
     exit
